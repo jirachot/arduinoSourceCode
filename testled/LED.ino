32541662c0a1ce62562deca323ae036dbafd77f4
Stick <jirachot23@gmail.com>
2014-08-01 16:22:22 +0700

    LED

---
+++ testled/LED.ino
@@ -0,0 +1,33 @@
+//header
+int led = 2;
+int led1 = 3;
+int led2 = 4;
+int led3 = 5;
+int led4 = 6;
+int led5 = 7;
+int led6 = 8;
+
+
+void setup(){
+  pinMode(led, OUTPUT);
+}
+void loop(){
+  digitalWrite(led, HIGH);
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+  digitalWrite(led6, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+  digitalWrite(led6, LOW);
+  delay(1000);
+  
+}
+
